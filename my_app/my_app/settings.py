974ce7051557a17770e016e6f230646df61fd278
Emmanuel <attahzuzu@gmail.com>
2023-05-09 17:26:53 +0100

    fixed bug

--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -74,17 +75,13 @@ WSGI_APPLICATION = 'my_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-   # 'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-   # }
-   'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'emmanuel',
-        'PASSWORD': 'batmanman',
-        'HOST':'db',
-        'PORT':'3306',
+   "default": {
+        "ENGINE": os.environ.get("ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("MYSQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("MYSQL_USER", "user"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", "password"),
+        "HOST": os.environ.get("MYSQL_HOST", "localhost"),
+        "PORT": os.environ.get("MYSQL_PORT", "3306"),
     }
 }
 
